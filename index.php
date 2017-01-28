a64391a575829dcf41ee26fb9980d0da724851da
RedMeca <tytydodo67140@gmail.com>
2017-01-28 17:43:48 +0100

    v1.0

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="style.css"/>
+        <title>Poups</title>
+    </head>
+    <body class="container">
+        <header class="container-fluid">
+            <div class="row">
+                <nav class="navbar navbar-default navbar-fixed-bottom">
+                    <div>
+                        <a></a>
+                    </div>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </nav>
+            </div>    
+        </header>
+        <div>
+        
+        </div>
+        <footer>
+        
+        </footer>
+    </body>
+</html>
